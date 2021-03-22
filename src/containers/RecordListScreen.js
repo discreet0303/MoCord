59a26e44ea182009fb38e59502f5b7d71d040452
Hsuan <qhespqhesp@gmail.com>
2021-03-22 21:45:17 +0800

    store record to json file and show in list screen

--- src/containers/RecordListScreen.js
+++ src/containers/RecordListScreen.js
@@ -1,21 +1,55 @@
 import React from 'react';
 import {View, Text, Button, StyleSheet} from 'react-native';
+import _ from 'lodash';
+import {getRecords, storeRecords} from '../utils/fileManager';
+import moment from 'moment';
 
 const styles = StyleSheet.create({
-  root: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
+  root: {},
 });
 
 const RecordListScreen = ({navigation}) => {
+  const [records, setRecords] = React.useState({});
+
+  React.useEffect(() => {
+    const runAsync = async () => {
+      const res = await getRecords();
+      setRecords(res);
+    };
+
+    runAsync();
+  });
+
+  const addRecord = async () => {
+    const temp = {
+      datetime: moment().format('YYYY-MM-DD HH:ss'),
+      type: 'food',
+      money: 100,
+      note: 'note1',
+    };
+    let updateRecords = [...records, temp];
+    setRecords(updateRecords);
+    console.log('updateRecords', updateRecords);
+
+    await storeRecords(updateRecords);
+  };
+
   return (
     <View style={styles.root}>
-      <Text>This is the recore list screen !</Text>
+      {_.map(records, (record, idx) => {
+        return (
+          <View key={idx}>
+            <Text>
+              {moment(record.datetime).format('HH:ss')} / {record.type} /
+              {record.money} / {record.note}
+            </Text>
+          </View>
+        );
+      })}
       <Button
         title="Go to Record Edit Screen"
-        onPress={() => navigation.navigate('RecordEdit')}
+        onPress={() => addRecord()}
+        // onPress={() => navigation.navigate('RecordEdit')}
       />
     </View>
   );
--- src/utils/fileManager.js
+++ src/utils/fileManager.js
@@ -1,13 +1,31 @@
-var RNFS = require('react-native-fs');
-
+import RNFS from 'react-native-fs';
 import {Platform} from 'react-native';
+import moment from 'moment';
+import _ from 'lodash';
 
 const dir = RNFS.ExternalDirectoryPath;
+const filename = moment().format('YYYY-MM-DD') + '.json';
+const filePath = dir + '/' + filename;
 
-export const storeRecord = () => {
-  const filePath = dir + '/123.txt';
+export const storeRecords = async (records) => {
+  await RNFS.writeFile(filePath, JSON.stringify(records), 'utf8')
+    .then((success) => {
+      console.log('FILE WRITTEN!');
+    })
+    .catch((err) => {
+      console.log(err.message);
+    });
+};
 
-  RNFS.writeFile(filePath, 'Lorem ipsum dolor sit amet', 'utf8')
+export const getRecords = async () => {
+  return await RNFS.readFile(filePath, 'utf8').then((res) => JSON.parse(res));
+};
+
+const data = [
+  {datetime: '2020-09-01 12:10', type: 'food', money: 100, note: 'note1'},
+  {datetime: '2020-09-02 12:10', type: 'drink', money: 200, note: 'note2'},
+  {datetime: '2020-09-03 12:10', type: 'home', money: 200, note: 'note3'},
+];
     .then((success) => {
       console.log('FILE WRITTEN!');
     })
