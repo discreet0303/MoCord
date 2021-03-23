28c60d4a70d7d580ad61d63460aa2c14950007d9
Hsuan <qhespqhesp@gmail.com>
2021-03-23 22:08:09 +0800

    add default record type

---
+++ src/config/DefaultRecordConfig.js
@@ -0,0 +1,9 @@
+export const DEFAULT_RECORD_TYPE = [
+  {name: 'food', label: '食物'},
+  {name: 'drink', label: '飲品'},
+  {name: 'transportation', label: '交通'},
+  {name: 'consumption', label: '消費'},
+  {name: 'home', label: '居家'},
+  {name: 'income', label: '收入'},
+  {name: 'others', label: '其他'},
+];
