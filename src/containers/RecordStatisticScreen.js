858a317d1a0190c69f8182ffe181f383a2fa0d31
MVP-Husan <chehsuan.chang@mvpfastlane.com>
2021-04-22 16:59:35 +0800

    finish refactor record statistic screen

--- src/containers/RecordStatisticScreen.js
+++ src/containers/RecordStatisticScreen.js
@@ -17,7 +17,7 @@ import { getRecordByMonth } from '../utils/fileManager';
 
 const styles = StyleSheet.create({
   root: {},
-  monthSection: { paddingVertical: 5 },
+  monthSection: { height: 50, paddingVertical: 5 },
   month: {
     width: 40,
     height: 40,
@@ -27,6 +27,18 @@ const styles = StyleSheet.create({
     borderRadius: 20,
     borderWidth: 1,
     borderColor: '#000',
+  },
+  summarySection: {
+    marginVertical: 10,
+  },
+  summaryRow: {
+    flexDirection: 'row',
+    paddingHorizontal: 10,
+    marginBottom: 5,
+  },
+  summaryRowText: {
+    fontSize: 18,
+    textAlign: 'right',
   },
   recordSection: {},
   recordItem: {
@@ -93,6 +105,43 @@ const RecordStatisticScreen = ({ navigation }) => {
     );
   };
 
+  const SummarySection = () => {
+    const groupByType = _.groupBy(monthReocrds, 'type');
+    let metadata = _.map(groupByType, (records, type) => {
+      const amount = _.sumBy(records, (item) => Number(item.money));
+      return {
+        records,
+        type,
+        amount,
+      };
+    });
+    const maxAmount = _.max(_.map(metadata, 'amount'));
+
+    return (
+      <View style={styles.summarySection}>
+        {_.map(metadata, (row) => (
+          <View key={row.type} style={styles.summaryRow}>
+            <Text style={styles.summaryRowText}>{row.type}</Text>
+            <View style={{ flex: 1, paddingHorizontal: 10, justifyContent: 'center' }}>
+              <View
+                style={{
+                  height: 1,
+                  backgroundColor: 'gray',
+                  width: `${(row.amount / maxAmount) * 100}%`,
+                }}
+              ></View>
+            </View>
+            <Text style={{ ...styles.summaryRowText, width: '15%' }}>{row.amount}</Text>
+          </View>
+        ))}
+        <View style={{ ...styles.recordAmount, marginHorizontal: 10 }}>
+          <Text style={[styles.recordText, { flex: 1 }]}>合計</Text>
+          <Text style={styles.recordText}>{_.sumBy(metadata, (item) => Number(item.amount))}</Text>
+        </View>
+      </View>
+    );
+  };
+
   const RecordSection = () => {
     let metadata = [];
     if (groupByMethod === 'type') metadata = _.groupBy(monthReocrds, 'type');
@@ -130,167 +179,24 @@ const RecordStatisticScreen = ({ navigation }) => {
     <SafeAreaView>
       <HeaderNav title={`${month} 月統計`} />
       <MonthSection />
-      <ScrollView>
-        <View style={{ flexDirection: 'row' }}>
-          <Button title={'類別'} onPress={() => setGroupByMethod('type')} />
-          <Button title={'備註'} onPress={() => setGroupByMethod('note')} />
+      {monthReocrds.length === 0 ? (
+        <View style={{ marginTop: 10 }}>
+          <Text style={{ textAlign: 'center', fontSize: 20 }}>無紀錄</Text>
         </View>
-        <RecordSection />
-      </ScrollView>
+      ) : (
+        <>
+          <SummarySection />
+          <ScrollView>
+            <View style={{ flexDirection: 'row' }}>
+              <Button title={'類別'} onPress={() => setGroupByMethod('type')} />
+              <Button title={'備註'} onPress={() => setGroupByMethod('note')} />
+            </View>
+            <RecordSection />
+          </ScrollView>
+        </>
+      )}
     </SafeAreaView>
   );
 };
 
 export default RecordStatisticScreen;
-
-// import React from 'react';
-
-// import { useSelector } from 'react-redux';
-
-// import HeaderNav from '../componments/HeaderNav';
-
-// import { getRecordByMonth } from '../utils/fileManager';
-// import { calcuTotalMoney } from '../utils/record';
-
-// const styles = StyleSheet.create({
-//   statisticRow: {
-//     flexDirection: 'row',
-//     justifyContent: 'center',
-//     paddingLeft: 15,
-//     paddingRight: 5,
-//     paddingVertical: 3,
-//   },
-//   statisticText: {
-//     fontSize: 16,
-//     width: 50,
-//   },
-// });
-
-// const RecordStatisticScreen = ({ navigation }) => {
-//   const year = moment().year();
-//   const [month, setMonth] = React.useState(moment().month() + 1);
-//   const [monthReocrd, setMonthRecord] = React.useState({});
-//   const [maxAmount, setMaxAmount] = React.useState(0);
-//   const types = useSelector((state) => state.types);
-
-//   React.useEffect(() => {
-//     const runAsync = async () => {
-//       const res = await getRecordByMonth(year, month);
-//       const total = calcuTotalMoney(res, types);
-//       let data = {
-//         total,
-//         records: {},
-//       };
-//       const groupByType = _.groupBy(res, 'type');
-//       _.each(groupByType, (typeData, type) => {
-//         const maxNum = _.sum(_.map(typeData, (d) => _.toInteger(d.money)));
-//         setMaxAmount((n) => {
-//           if (n < maxNum) return maxNum;
-//           return n;
-//         });
-//         data.records[type] = {
-//           amount: maxNum,
-//           label: type,
-//           records: typeData,
-//         };
-//       });
-//       setMonthRecord(data);
-//     };
-//     runAsync();
-//   }, [month]);
-
-//   return (
-//     <SafeAreaView>
-//       <HeaderNav title={`${month} 月統計`} />
-//       <ScrollView>
-//         {_.map(monthReocrd.records, (typeData, type) => {
-//           return (
-//             <View key={type} style={styles.statisticRow}>
-//               <Text style={styles.statisticText}>{typeData.label}</Text>
-//               <View
-//                 style={{
-//                   flex: 1,
-//                   justifyContent: 'center',
-//                   paddingRight: 20,
-//                 }}
-//               >
-//                 <View
-//                   style={{
-//                     height: 1,
-//                     backgroundColor: 'gray',
-//                     width: `${_.toInteger((typeData.amount / maxAmount) * 100)}%`,
-//                   }}
-//                 ></View>
-//               </View>
-//               <Text style={styles.statisticText}>{typeData.amount}</Text>
-//             </View>
-//           );
-//         })}
-//         <View style={{ height: 1, backgroundColor: '#b7b7b7' }}></View>
-//         <View style={styles.statisticRow}>
-//           <Text style={styles.statisticText}>合計</Text>
-//           <View
-//             style={{
-//               flex: 1,
-//               justifyContent: 'center',
-//               paddingRight: 20,
-//             }}
-//           ></View>
-//           <Text style={styles.statisticText}>{monthReocrd.total}</Text>
-//         </View>
-//         <View
-//           style={{
-//             backgroundColor: '#ffffff',
-//             shadowColor: '#000',
-//             shadowOffset: {
-//               width: 0,
-//               height: 1,
-//             },
-//             shadowOpacity: 0.2,
-//             shadowRadius: 1.41,
-//             elevation: 2,
-//             marginBottom: 20,
-//           }}
-//         >
-//           <Text style={{ textAlign: 'center', fontSize: 30 }}>All Record</Text>
-//           {_.map(monthReocrd.records, (typeData, type) => {
-//             return (
-//               <View key={type} style={{  }}>
-//                 <Text
-//                   style={{
-//                   }}
-//                 >
-//                   {typeData.label}
-//                 </Text>
-//                 {_.map(typeData.records, (record, idx) => (
-//                   <View key={idx} style={{ flexDirection: 'row' }}>
-//                     <Text style={{ fontSize: 18, paddingRight: 15 }}>
-//                       {moment(record.datetime).format('DD')}
-//                     </Text>
-//                     <Text style={{ flex: 1, fontSize: 18 }}>{record.note}</Text>
-//                     <Text style={{ fontSize: 18 }}>${record.money}</Text>
-//                   </View>
-//                 ))}
-//                 <View
-//                   style={{
-//                     flexDirection: 'row',
-//                     marginTop: 5,
-//                     borderTopColor: '#b7b7b7',
-//                     borderTopWidth: 1,
-//                   }}
-//                 >
-//                   <Text style={{ flex: 1, fontSize: 18 }}>合計</Text>
-//                   <Text style={{ fontSize: 18 }}>
-//                     {_.sum(_.map(typeData.records, (r) => _.toInteger(r.money)))}
-//                   </Text>
-//                 </View>
-//               </View>
-//             );
-//           })}
-//         </View>
-//       </ScrollView>
-//     </SafeAreaView>
-//   );
-// };
-
-// export default RecordStatisticScreen;
